040ebbf72bb44662c4653aa0fdd51e1d164d5380
John W. Long <john@uservoice.com>
2015-09-26 15:00:50 -0400

    Hide completed items; show total number of incomplete tasks

--- meteor-tutorial.js
+++ meteor-tutorial.js
@@ -1,4 +1,4 @@
-/* globals Meteor, Mongo, Template */
+/* globals Meteor, Mongo, Template, Session */
 
 var Tasks = new Mongo.Collection("tasks");
 
@@ -6,7 +6,19 @@ if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
     tasks: function() {
-      return Tasks.find({}, {sort: {createdAt: -1}});
+      if (Session.get("hideCompleted")) {
+        // If hide completed is checked, filter tasks
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      } else {
+        // Otherwise, return all of the tasks
+        return Tasks.find({}, {sort: {createdAt: -1}});
+      }
+    },
+    hideCompleted: function () {
+      return Session.get("hideCompleted");
+    },
+    incompleteCount: function () {
+      return Tasks.find({checked: {$ne: true}}).count();
     }
   });
 
@@ -26,6 +38,9 @@ if (Meteor.isClient) {
  
       // Clear form
       event.target.text.value = "";
+    },
+    "change .hide-completed input": function (event) {
+      Session.set("hideCompleted", event.target.checked);
     }
   });
 
